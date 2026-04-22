4e647c311945821127415cc5073909f59062f5a6
simeon <simmeon.nv@proton.me>
2026-04-22 09:38:00 +0300

    robots txt + favicon + Register now

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+useHead({
+  title: "LiberHack",
+  link: [{ rel: "icon", href: "/favicon.webp" }],
+});
+
+useSeoMeta({
+  description: "The LiberHack burgas hackaton.",
+  ogTitle: "LiberHack",
+  ogDescription: "The LiberHack burgas hackaton",
+  ogImage: "/favicon.webp",
+  twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
+  <div
+    class="w-full h-96 flex flex-row justify-center align-middle items-center gap-8 p-8"
+  >
+    <RegisterNow />
+    <NuxtImg
+      src="/favicon.webp"
+      class="bg-base-100/90 border-primary border-4"
+    />
+  </div>
   <p>hallo</p>
 </template>
