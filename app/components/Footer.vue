61e12f9b73c8fe847ac050a8334ccd6c18a05621
simeon <simmeon.nv@proton.me>
2026-04-22 10:53:21 +0300

    footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer
+    class="footer sm:footer-horizontal bg-base-100 text-neutral-content gap-0 border-2 border-primary mb-4"
+  >
+    <aside class="flex flex-row items-center border-2 border-primary p-2">
+      <NuxtImg src="/favicon.webp" class="w-32 border-primary border-r-2" />
+      <p>LiberHack.</p>
+    </aside>
+
+    <div
+      class="border-primary border-2 h-full p-2 md:place-self-center md:justify-self-end w-full md:w-min"
+    >
+      <h1 class="text-3xl w-full">Social</h1>
+      <div class="grid grid-flow-col gap-4 h-full w-full">
+        <a
+          class="flex flex-row gap-2 items-center"
+          href="https://www.instagram.com/liberh4ck/"
+        >
+          <p>Instagram</p>
+          <Icon
+            size="32"
+            name="streamline-pixel:logo-social-media-old-instagram"
+          />
+        </a>
+        <a class="flex flex-row gap-2 items-center">
+          <p>Discord</p>
+          <Icon size="32" name="teenyicons:discord-solid" />
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,29 @@
 <template>
-  <div
-    class="w-full flex flex-col justify-center align-middle items-center gap-8 p-8"
-  >
-    <p class="text-5xl font-bold">Fr3qu3nt!y A5k3d Qu3st10n5</p>
-    <div class="flex flex-row w-full gap-8">
+  <div class="w-full flex flex-col gap-16">
+    <div
+      class="w-full flex flex-col justify-center align-middle items-center gap-8 p-8"
+    >
+      <p class="text-5xl font-bold">Fr3qu3nt!y A5k3d Qu3st10n5</p>
+      <div class="flex flex-row w-full gap-8">
+        <NuxtImg
+          src="/favicon.webp"
+          class="bg-base-100/90 border-primary border-4 hidden md:block"
+        />
+        <FAQ />
+      </div>
+    </div>
+
+    <div
+      class="w-full h-96 flex flex-row justify-center align-middle items-center gap-8 p-8"
+    >
+      <RegisterNow />
       <NuxtImg
         src="/favicon.webp"
         class="bg-base-100/90 border-primary border-4 hidden md:block"
       />
-      <FAQ />
     </div>
+    <p>hallo</p>
+
+    <Footer />
   </div>
-  <div
-    class="w-full h-96 flex flex-row justify-center align-middle items-center gap-8 p-8"
-  >
-    <RegisterNow />
-    <NuxtImg
-      src="/favicon.webp"
-      class="bg-base-100/90 border-primary border-4 hidden md:block"
-    />
-  </div>
-  <p>hallo</p>
 </template>
