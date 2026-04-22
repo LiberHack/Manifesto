1f8aca3ce3646dc8aa3fa9de71431a3ee55e99e5
simeon <simmeon.nv@proton.me>
2026-04-22 10:18:54 +0300

    faq

---
+++ app/components/FAQ.vue
@@ -0,0 +1,114 @@
+<template>
+  <ul class="list bg-base-100 rounded-box shadow-md w-full">
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">awards</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">team_size</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">register_time</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">is_alone</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">location</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">work_in_advance</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">tech_stack</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">food</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">from_home</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">mentors</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+    <li
+      tabindex="0"
+      class="collapse collapse-arrow bg-base-100 border-base-300 border"
+    >
+      <div class="collapse-title font-semibold">more_questions</div>
+      <div class="collapse-content text-sm">
+        halka halka halkhalka halka halkhalka halka halkhalka halka halkhalka
+        halka halkhalka halka halkhalka halka halk
+      </div>
+    </li>
+  </ul>
+</template>
