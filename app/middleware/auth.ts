52a6e3523f94ca80717aa643937080815b6c1af8
Kirill Ibragimov <hexchap@gmail.com>
2026-04-24 14:07:33 +0300

    fix: auth middleware to check correct field

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const user = useSupabaseUser();
   if (!user.value) return navigateTo("/ops/login");
-  if (!user.value.email_confirmed_at) return navigateTo("/ops/verify-email");
+  if (!user.value.user_metadata?.email_verified)
+    return navigateTo("/ops/verify-email");
 });
