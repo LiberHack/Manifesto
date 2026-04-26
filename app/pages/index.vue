36365c451b4458a4d5dbe4d8c6186e951a71dc9b
simeon <simmeon.nv@proton.me>
2026-04-26 19:59:52 +0300

    index done

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex flex-col gap-16 py-8 font-cygrotesk">
+  <div class="w-full flex flex-col gap-16 py-8 font-cygrotesk md:px-32">
     <div
       class="w-full flex flex-col md:flex-row items-center justify-center gap-5"
     >
@@ -18,18 +18,48 @@
     </div>
 
     <div class="w-full flex flex-col gap-8 items-center">
-      <p class="text-6xl font-bold">Ass0c1at3s</p>
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-        <div v-for="i in 4" class="flex flex-col items-center gap-2">
-          <NuxtImg
-            preload
-            src="/favicon.webp"
-            class="bg-base-100/90 border-primary border-4"
-          />
+      <p class="text-6xl font-bold items-stretch">Ass0c1at3s</p>
+      <div class="flex flex-col lg:flex-row gap-16">
+        <div class="flex flex-col items-center gap-2">
+          <div>
+            <NuxtImg
+              preload
+              src="/burgaslab.png"
+              class="bg-base-100/90 border-primary border-4 h-48 w-full"
+            />
+          </div>
           <p
-            class="font-bold text-xl md:text-2xl bg-base-100/80 border-primary border-2"
+            class="font-bold text-xl md:text-2xl bg-base-100/80 border-primary border-2 p-2"
           >
-            {SUBJECT NAME HERE}
+            BurgasLab
+          </p>
+        </div>
+        <div class="flex flex-col items-center gap-2">
+          <div>
+            <NuxtImg
+              preload
+              src="/ppmg.jpg"
+              class="bg-base-100/90 border-primary border-4 h-48 w-full"
+            />
+          </div>
+          <p
+            class="font-bold text-xl md:text-2xl bg-base-100/80 border-primary border-2 p-2"
+          >
+            PPMG
+          </p>
+        </div>
+        <div class="flex flex-col items-center gap-2">
+          <div>
+            <NuxtImg
+              preload
+              src="/vladan.jpg"
+              class="bg-base-100/90 border-primary border-4 h-48 w-full"
+            />
+          </div>
+          <p
+            class="font-bold text-xl md:text-2xl bg-base-100/80 border-primary border-2 p-2"
+          >
+            VLADAN OOD
           </p>
         </div>
       </div>
@@ -41,25 +71,27 @@
       <p class="text-3xl lg:text-5xl font-bold text-center">
         Fr3qu3nt!y A5k3d Qu3st10n5
       </p>
-      <div class="flex flex-col lg:flex-row w-full gap-8">
-        <NuxtImg
-          preload
-          src="/favicon.webp"
-          class="bg-base-100/90 border-primary border-4"
-        />
+      <div class="flex flex-col lg:flex-row w-full p-8 gap-8 md:gap-24">
+        <div class="flex items-center justify-center">
+          <NuxtImg
+            preload
+            src="/pc.webp"
+            class="bg-base-100/90 border-primary border-4 rendering-pixelated w-lg"
+          />
+        </div>
 
         <FAQ class="h-min" />
       </div>
     </div>
 
     <div
-      class="w-full h-96 flex flex-row justify-center align-middle items-center gap-8 p-8"
+      class="w-full h-96 flex flex-row justify-center align-middle items-center gap-8 p-8 md:p-24"
     >
       <RegisterNow />
       <NuxtImg
         preload
-        src="/favicon.webp"
-        class="bg-base-100/90 border-primary border-4 hidden md:block"
+        src="/eyes.webp"
+        class="bg-base-100/90 border-primary border-4 w-xs hidden md:block"
       />
     </div>
 
