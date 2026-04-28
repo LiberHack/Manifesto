afe8321805ee2fb22619dd80a6edd0dc813dfa46
Kirill Ibragimov <hexchap@gmail.com>
2026-04-29 02:35:55 +0300

    fix: add url declarations to the header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,8 @@
 <script setup>
 const route = useRoute();
+const appConfig = useAppConfig();
+const discord = appConfig.socials.discord;
+const instagram = appConfig.socials.instagram;
 </script>
 
 <template>
@@ -7,31 +10,20 @@ const route = useRoute();
     <ScanlineShader />
   </ClientOnly>
 
-  <div
-    class="shader-container w-full min-h-screen bg-[url('/index.webp')] bg-repeat bg-auto font-cygrotesk"
-  >
+  <div class="shader-container w-full min-h-screen bg-[url('/index.webp')] bg-repeat bg-auto font-cygrotesk">
     <div class="flex flex-col min-h-screen">
-      <div
-        class="navbar bg-base-100 shadow-sm border-b-2 border-primary gap-2 px-4"
-      >
+      <div class="navbar bg-base-100 shadow-sm border-b-2 border-primary gap-2 px-4">
         <div class="">
-          <NuxtLink
-            to="/"
-            class="btn btn-ghost text-xs md:text-xl font-cy"
-            :class="{ 'btn-active btn-primary': route.path === '/' }"
-          >
+          <NuxtLink to="/" class="btn btn-ghost text-xs md:text-xl font-cy"
+            :class="{ 'btn-active btn-primary': route.path === '/' }">
             L1BERH4CK
           </NuxtLink>
         </div>
 
         <div class="">
-          <NuxtLink
-            to="/ops/teams"
-            class="btn btn-ghost font-cy text-xs md:text-xl"
-            :class="{
-              'btn-active btn-primary': route.path.startsWith('/ops') === true,
-            }"
-          >
+          <NuxtLink to="/ops/teams" class="btn btn-ghost font-cy text-xs md:text-xl" :class="{
+            'btn-active btn-primary': route.path.startsWith('/ops') === true,
+          }">
             OPS
           </NuxtLink>
         </div>
@@ -39,23 +31,12 @@ const route = useRoute();
         <div class="grow" />
 
         <div class="flex flex-row gap-4 h-full flex-none items-center">
-          <a
-            class="flex flex-row gap-2 items-center"
-            :href="instagram"
-            target="_blank"
-          >
+          <a class="flex flex-row gap-2 items-center" :href="instagram" target="_blank">
             <p class="hidden sm:block">Instagram</p>
-            <Icon
-              size="32"
-              name="streamline-pixel:logo-social-media-old-instagram"
-            />
+            <Icon size="32" name="streamline-pixel:logo-social-media-old-instagram" />
           </a>
 
-          <a
-            :href="discord"
-            class="flex flex-row gap-2 items-center"
-            target="_blank"
-          >
+          <a :href="discord" class="flex flex-row gap-2 items-center" target="_blank">
             <p class="hidden sm:block">Discord</p>
             <Icon size="32" name="teenyicons:discord-solid" />
           </a>
