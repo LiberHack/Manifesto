b29a51251fb46888021fcf41ed2bb852e28d2820
Kirill Ibragimov <hexchap@gmail.com>
2026-04-29 02:35:55 +0300

    fix: add url declarations to the header

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,14 +1,11 @@
 <template>
-  <div
-    class="fixed inset-0 w-screen h-screen gap-2 flex flex-col items-center justify-center"
-  >
+  <div class="fixed inset-0 w-screen h-screen gap-2 flex flex-col items-center justify-center">
     <div
-      class="bg-base-100/60 p-2 flex flex-row gap-10 items-center justify-center text-6xl font-bold text-red-500"
-    >
+      class="bg-base-100/60 p-2 flex flex-row gap-10 items-center justify-center text-xl font-bold text-red-500 md:text-6xl">
       <p>YOU SHOULDN'T BE HERE</p>
       <p>404</p>
     </div>
-    <div class="bg-base-100/60 text-3xl font-bold">
+    <div class="bg-base-100/60 text-lg font-bold md: text-3xl">
       <NuxtLink href="/">back</NuxtLink>
     </div>
   </div>
